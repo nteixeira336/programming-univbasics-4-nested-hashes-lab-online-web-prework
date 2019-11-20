8f1e9dde48c668c3d56ed40ae74fb8e1e999929b
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-11-20 15:33:02 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,12 +15,15 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
-end
+     
+    
+    end
+    
+    def hopper_is_known_for
+      programmer_hash[:grace_hopper][:known_for][:languages]
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	programmer_hash= [:allan_kay][:known_for]
 	
 	programmer_hash = 
  		{
@@ -40,6 +43,8 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
+  programmer_hash[:dennis_ritchie][:languages]
+   
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -63,7 +68,10 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
+programmer_hash.unshift (":yukihiro_matsumoto" => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"] 
+})
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -101,7 +109,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for]= "GUI"
      
 end
 
--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -4,6 +4,7 @@ require_relative "../nested.rb"
 describe "nested hash" do
   describe "hopper" do
     it "operates on the programmer_hash and returns the value of the :grace_hopper key" do
+      
       expect(hopper).to eq({:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]})
 
     end

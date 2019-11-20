5b007798b40134f0dbca5dd13fe1ccf582d32b56
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-11-20 15:30:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,7 +68,10 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
+programmer_hash.unshift (":yukihiro_matsumoto" => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"] 
+})
 	programmer_hash = 
  		{
         :grace_hopper => {

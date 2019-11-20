d1420b3690314a706275a6998f4f32d4d9071d93
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-11-20 15:06:13 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -4,7 +4,6 @@ require_relative "../nested.rb"
 describe "nested hash" do
   describe "hopper" do
     it "operates on the programmer_hash and returns the value of the :grace_hopper key" do
-      binding.pry
       expect(hopper).to eq({:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]})
 
     end
